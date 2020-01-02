f46f0ee2e0544cbeb3cf314651467343fc17c488
Manuel Bugallo <marvinody@gmail.com>
2020-01-02 03:57:43 -0500

    feat: generates an initial version of the html

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
 const yaml = require("js-yaml")
 const fs = require("fs")
 const path = require("path")
+const mkdirp = require("mkdirp")
+const templates = require("./templates")
 const join = path.join
 
 const file = process.argv[2]
@@ -12,22 +14,70 @@ const filepath = path.join(process.cwd(), file)
 
 // Get document, or throw exception on error
 try {
+  // load the file from args
   const doc = yaml.safeLoad(fs.readFileSync(filepath, "utf8"))
+
+  // pull some general stuff for later
   const year = String(doc.year)
+  // this is for filesystem
   const baseHTMLFolder = join(__dirname, year)
-  const baseImagesFolder = join(__dirname, "images", year)
+  // this is for web (so root is here)
+  const baseImagesFolder = join(path.sep, "images")
+
   doc.list.forEach((person, idx, arr) => {
+    // grab folder names now and pass them down
     const name = person.name.toLowerCase()
     const htmlFolder = join(baseHTMLFolder, name)
     const paddedIdx = String(idx).padStart(2, "0")
-    const imageFolder = join(baseHTMLFolder, `${paddedIdx}-${name}`)
-    makePost({ person, htmlFolder, imageFolder })
+    const imageFolder = join(baseImagesFolder, year, `${paddedIdx}-${name}`)
+    const thumbFolder = join(
+      baseImagesFolder,
+      "thumbs",
+      year,
+      `${paddedIdx}-${name}`
+    )
+
+    // make the html for each person now. this takes care of file formation also
+    makePage({ person, htmlFolder, imageFolder, thumbFolder })
   })
-  console.log()
 } catch (e) {
   console.log(e)
 }
 
-function makePost({ person, htmlFolder, imageFolder }) {
-  console.log({ person, htmlFolder, imageFolder })
+// generate page for given person.
+function makePage({ person, htmlFolder, imageFolder, thumbFolder }) {
+  // where are we putting the final file
+  const htmlFile = join(htmlFolder, "index.html")
+  const entries = person.entries
+  // for each entry, 'switch' on whatever it looks like
+  const posts = entries
+    .map(entry => {
+      // had these as variables before but join yells if undefined args
+      const getPics = () => [
+        join(imageFolder, entry.pic),
+        join(thumbFolder, entry.pic),
+      ]
+
+      if (entry.divider) {
+        return templates.divider()
+      } else if (entry.pic && entry.text) {
+        const [image, thumb] = getPics()
+        return templates.captionedImage({
+          image,
+          thumb,
+          alt: entry.text,
+        })
+      } else if (entry.pic) {
+        const [image, thumb] = getPics()
+        return templates.image({ image, thumb })
+      } else if (entry.text) {
+        return templates.message(entry.text)
+      }
+    })
+    .join("") // everything is a string in our template world
+
+  // TODO fix nav later (raises interesting passing issues)
+  const page = templates.page({ person, nav: "", posts })
+  mkdirp.sync(htmlFolder)
+  fs.writeFileSync(htmlFile, page)
 }
